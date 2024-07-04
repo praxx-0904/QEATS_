4f42bc427cbd5dacbd7a7b65dfb599261630c8c8
praxx-0904 <105570181+praxx-0904@users.noreply.github.com>
2024-07-04 17:21:06 +0530

    Update Restaurant.java

--- src/main/java/com/crio/qeats/dto/Restaurant.java
+++ src/main/java/com/crio/qeats/dto/Restaurant.java
@@ -1,12 +1,6 @@
-
-/*
- *
- *  * Copyright (c) Crio.Do 2019. All rights reserved
- *
- */
-
 package com.crio.qeats.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +9,55 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Restaurant {
 
+    @NotNull
+    private String restaurantId;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String city;
+
+    private String imageUrl;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @NotNull
+    private String opensAt;
+
+    @NotNull
+    private String closesAt;
+
+    private List<String> attributes = new ArrayList<>();
+
+    // Getters and setters for all fields
+
+    public void addAttribute(String attribute) {
+        this.attributes.add(attribute);
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
 
